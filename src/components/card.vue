2fbe2d654a1d5fad97eb9beb2f82b8ee894b1d99
ValerioCarbone <valeriocarbone10@gmail.com>
2023-11-13 12:10:46 +0100

    ADD - content style

---
+++ src/components/card.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+    props: {
+        item: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+<template>
+    <ul class="col-6">
+        <li v-if="item.original_title">{{ item.original_title }}</li>
+        <li v-if="item.original_name">{{ item.original_name }}></li>
+        <li v-if="item.title">{{ item.title }}</li>
+        <li v-if="item.name">{{ item.name }}</li>
+        <li>{{ item.original_language }}</li>
+        <li>{{ item.vote_average }}</li>
+    </ul>
+</template>
+<style scoped lang="scss">
+ul,
+ol,
+menu {
+    list-style: none;
+}
+
+.col-6 {
+    flex-basis: 50%;
+}
+</style>
--- src/components/pageMain.vue
+++ src/components/pageMain.vue
@@ -1,11 +1,41 @@
 <script>
+import card from './card.vue'
+import { store } from '../store'
 export default {
+    data() {
+        return {
+            store
+        }
+    },
+    components: {
+        card
+    },
+    computed: {
+        movies() {
+            return this.store.movies
+        },
+        series() {
+            return this.store.series
+        }
+    }
 }
 </script>
 
 <template>
     <main class="page-main">
         <div class="container">
+            <div class="col-6 card-container">
+                <h2 class="card-title">Movies</h2>
+                <div class="movies">
+                    <card class="col-6" v-for="movie in movies" :item="movie" />
+                </div>
+            </div>
+            <div class="col-6 card-container">
+                <h2 class="card-title">Series</h2>
+                <div class="series">
+                    <card v-for="serie in series" :item="serie" />
+                </div>
+            </div>
         </div>
     </main>
 </template>
@@ -13,5 +43,29 @@ export default {
 <style scoped lang="scss">
 .container {
     background-color: rgb(41, 40, 40);
+    display: flex;
+}
+
+.col-6 {
+    flex-basis: calc(100% / 2);
+}
+
+.card-title {
+    text-align: center;
+    color: greenyellow;
+}
+
+.movies,
+.series {
+    display: flex;
+    color: white;
+    flex-wrap: wrap;
+    padding: 20px;
+}
+
+.card-container {
+    flex-shrink: 0;
+    display: flex;
+    flex-direction: column;
 }
 </style>
